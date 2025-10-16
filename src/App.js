200b9dcd21432aaddcde0c16b21ab8a8dfa3b5eb
geoffreyleroux <jojo45452@gmail.com>
2025-10-16 20:33:43 +0200

    a

--- src/App.js
+++ src/App.js
@@ -7,6 +7,47 @@ const styles = `
       background-color: black;
       font-family: "Jost";
     }
+    
+    .video {
+      margin-bottom: 40px;
+      width: 900px;
+      height: 600px;
+      maxWidth: 100%;
+      maxHeight: 100%;
+    }
+    .text-container {
+      display: flex;
+      flex-direction: row;
+      text-align: center;
+    }
+    .main-container {
+      display: flex;
+      flex: 0 1 auto;
+      align-content: center;
+      alignitems: center;
+      flex-direction: row;
+      max-width: 100%;
+      max-height: 100%;
+    }
+
+    @media only screen and (max-width: 600px) {
+      .video {
+        width: 100%;
+        height: auto;
+        max-width: 100%;
+        max-height: 100%;
+      }
+      * {
+        font-size: 9px
+      }
+      .text-container {
+      
+      }
+
+      .main-container {
+      
+      }
+    }
 `;
 
 export default function App() {
@@ -14,6 +55,7 @@ export default function App() {
     <div className="exhibition">
       <style>{styles}</style>
       <div
+        className="main-container"
         style={{
           display: "flex",
           flex: "1 1 auto",
@@ -23,32 +65,11 @@ export default function App() {
           height: "100vh",
         }}
       >
-        <video
-          style={{
-            marginTop: "20px",
-            marginBottom: "40px",
-            display: "flex",
-            flex: "0 1 auto",
-            alignContent: "center",
-            alignItems: "center",
-            flexDirection: "row",
-          }}
-          width="900"
-          height="600"
-          controls
-          autoplay
-          loop
-        >
+        <video className="video" controls autoplay loop>
           <source src="TOUTANOU2.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            textAlign: "center",
-          }}
-        >
+        <div className="text-container">
           <div
             style={{
               display: "flex",
@@ -74,10 +95,18 @@ export default function App() {
                 textAlign: "left",
               }}
             >
-              <p>doorikwak.fr@gmail.com <b>@d0ori</b></p>
-              <p>ryeorinkim@gmail.com <b>@libbyseaweed</b></p>
-              <p>newsymind@gmail.com <b>@notoldsoyoung</b></p>
-              <p>nari.gn@gmail.com <b>@memeno</b></p>
+              <p>
+                doorikwak.fr@gmail.com <b>@d0ori</b>
+              </p>
+              <p>
+                ryeorinkim@gmail.com <b>@libbyseaweed</b>
+              </p>
+              <p>
+                newsymind@gmail.com <b>@notoldsoyoung</b>
+              </p>
+              <p>
+                nari.gn@gmail.com <b>@memeno</b>
+              </p>
             </div>
           </div>
         </div>
